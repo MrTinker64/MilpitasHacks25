825b207ba1a0a5bc40f29f86fb01c7df83caa83b
MrTinker64 <avi.micah@gmail.com>
2025-05-24 10:27:52 -0700

    moving route

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -2,12 +2,12 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 from flask import send_from_directory
 
+app = Flask(__name__)
+CORS(app)
+
 @app.route('/')
 def serve():
     return send_from_directory('react-frontend/build', 'index.html')
-
-app = Flask(__name__)
-CORS(app)
 
 @app.route('/api/data', methods=['GET'])
 def get_data():
