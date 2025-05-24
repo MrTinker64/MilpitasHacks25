cedcb212b77d52bdad6fbeeacd7758af57886e55
MrTinker64 <avi.micah@gmail.com>
2025-05-24 10:01:31 -0700

    add send from directory

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -1,5 +1,10 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from flask import send_from_directory
+
+@app.route('/')
+def serve():
+    return send_from_directory('react-frontend/build', 'index.html')
 
 app = Flask(__name__)
 CORS(app)
