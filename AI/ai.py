d44ab73600b8a30053b8642473d69bc0bc1373ab
AUDREY TU <audreywtu@gmail.com>
2025-05-24 16:35:56 -0700

    ai function w/ location

--- AI/ai.py
+++ AI/ai.py
@@ -17,31 +17,35 @@ os.environ['GOOGLE_API_KEY'] = os.getenv('GOOGLE_API_KEY', geminikey)
 gpt = OpenAI()
 gemini = genai.Client(api_key = os.environ['GOOGLE_API_KEY'])
 
+long = "37.4323° N"
+lat = "121.8996° W"
+
 system_message = "You are part of an app that helps inform people of natural disasters"
-user_prompt = "Put together a list of supplies for a natural disaster emergency kit. Respond as json with item name, description, quantity, and expiration. For expiration, responses should be as quantitative as possible: a specific amount of time, not range (if applicable) is ideal. If an entry consists of multiple items with differnt expiration dates, state the most recent one. Also, if applicable, provide amazon search links where each item can be obtained."
+user_prompt = "Put together a list of supplies for an emergency kit, in preparation for a local natural disaster (based on the user's location of", long, lat + "). Respond as json with item name, description, quantity, and expiration. For expiration, responses should be as quantitative as possible: a specific amount of time, not range (if applicable) is ideal. If an entry consists of multiple items with differnt expiration dates, state the most recent one. Also, if applicable, provide amazon search links where each item can be obtained."
 
 prompts = [
     {"role": "system", "content": system_message},
     {"role": "user", "content": user_prompt}
   ]
 
-temperature = 0.3
+temp = 0.3
 
-#I generally prefer GPT's responses to Gemini's, since they're more consise, but I wrote in the code for both (if you wanted options)
-completion = gpt.chat.completions.create(
-    model="gpt-4o",
-    messages=prompts,
-    temperature=temperature
-)
-response = completion.choices[0].message.content
-delim = '+++++++++++++++++++'
-print(delim, "GPT", delim)
-print(response)
+def call_gpt(promptsss):
+    completion = gpt.chat.completions.create(
+        model="gpt-4o",
+        messages=promptsss
+        # temperature=temp
+    )
+    response = completion.choices[0].message.content
+    return "GPT:", response
 
-response = gemini.models.generate_content(
-    model="gemini-2.0-flash",
-    contents=user_prompt,
-    #temperature=temperature
-)
-print(delim, "Gemini", delim)
-print(response.text)
+def call_gemini(user_prompttt):
+   response = gemini.models.generate_content(
+      model="gemini-2.0-flash",
+      contents=[f"{user_prompttt}"]
+      #temperature=temp
+   )
+   return f"Gemini: {response}"
+
+#print(call_gpt(prompts))
+print(call_gemini(user_prompt))
